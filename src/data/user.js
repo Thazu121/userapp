0cfa26867ed685fb0fcb1eaf4df06e3d634bac72
Thasleema <thasleemapk133@gmail.com>
2026-02-11 15:49:33 +0530

    added the mock data file

---
+++ src/data/user.js
@@ -0,0 +1,39 @@
+const users = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@example.com",
+    phone: "9876543210",
+    city: "New York"
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    email: "jane@example.com",
+    phone: "9123456780",
+    city: "London"
+  },
+  {
+    id: 3,
+    name: "Rahul",
+    email: "rahul@example.com",
+    phone: "9988776655",
+    city: "Delhi"
+  },
+    {
+    id: 4,
+    name: "Ramesh",
+    email: "ramesh@example.com",
+    phone: "9988554321",
+    city: "Delhi"
+  },
+    {
+    id: 5,
+    name: "Ajas",
+    email: "ajas@example.com",
+    phone: "9988774322",
+    city: "chennai"
+  }
+];
+
+export default users;
