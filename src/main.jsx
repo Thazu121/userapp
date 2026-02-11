72d7b6f0cb9ac626c1cb695cf95e8157a0711e62
Thasleema <thasleemapk133@gmail.com>
2026-02-11 22:05:00 +0530

    added gh in package.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,29 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App.jsx'
-import RootLayout from "./Layouts/RootLayout.jsx"
-import {createBrowserRouter,RouterProvider,HashRouter} from "react-router-dom"
-import Home from "./components/pages/Home.jsx"
-import About from "./components/pages/About.jsx"
+
+import RootLayout from './Layouts/RootLayout.jsx'
+import Home from './components/pages/Home.jsx'
+import About from './components/pages/About.jsx'
 import Users from './components/pages/Users.jsx'
 import UserDetails from './components/pages/UserDetails.jsx'
-import SingleUserLoader from './Loaders/SingleUserLoader.jsx'
+
 import UsersLoader from './Loaders/UserLoader.jsx'
-
-
+import SingleUserLoader from './Loaders/SingleUserLoader.jsx'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <RootLayout />,
     children: [
       { index: true, element: <Home /> },
-      { path: "about", element: <About /> },
-      { path: "users", element: <Users /> ,loader:UsersLoader },
-      {path:"users/:userid" ,element:<UserDetails />,loader:SingleUserLoader}
-
-
-    ]
-  },],
-
-)
-
+      { path: 'about', element: <About /> },
+      { path: 'users', element: <Users />, loader: UsersLoader },
+      { path: 'users/:userid', element: <UserDetails />, loader: SingleUserLoader },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
